b67b3c73eecbe5ddd8b190865d2579ae5fa246f6
inesgarper <igperianez@gmail.com>
2022-02-16 19:03:36 +0100

    add admited comments

--- public/js/editProfile.js
+++ public/js/editProfile.js
@@ -2,6 +2,8 @@ const form = document.querySelector('#edit-form')
 
 const editButton = document.getElementById('edit-user-btn')
 
+console.log(editButton)
+
 editButton.addEventListener('click', function (event) {
     form.classList.toggle('editUser-on')
 })
--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     userIs: user => user.role === 'USER',
     userIsEditor: user => user.role === 'EDITOR',
-    userIsAdmin: user => user.role === 'ADMIN', 
-    userIsSelf: (user, loggedId) => user === loggedId
+    userIsAdmin: user => user.role === 'ADMIN',
+    userIsSelf: (user, loggedId) => user === loggedId,
+    commentsAreAdmitted: caso => caso.admitteComments === 'on'
 }
